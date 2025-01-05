b24170290ab484bc56e997668b8595f03b60dc11
Christian Molinari <chmolinari@christianmolinari.com>
2025-01-05 21:38:23 +0100

    Lists
    Created my first list component.
    
    Signed-off-by: Christian Molinari <chmolinari@christianmolinari.com>

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,17 +1,21 @@
 import './App.css'
 
-let tech = "React";
-let face = "😊";
-let author = "C. Molinari";
-let year = 2025;
+const tech = "React";
+const face = "😊";
+const author = "C. Molinari";
+const year = 2025;
+
+const advantages = [
+    "Components simplify the development process.",
+    "Widely used technology, it's useful to learn it.",
+    "Easy to learn and use."
+];
 
 function App() {
     return (
         <>
             <Header tech={tech} year="2025" author={author}/>
-            <main>
-                <h2>First steps into React development</h2>
-            </main>
+            <Main advantages={advantages}/>
         </>
     );
 }
@@ -22,6 +26,19 @@ function Header({tech, year, author}) {
             <h1>Hello {tech.toUpperCase()}!!! {face}</h1>
             <p>Copyright &#169; {year} {author}</p>
         </header>
+    );
+}
+
+function Main({advantages}) {
+    return (
+        <main>
+            <h2>Advantages of using React:</h2>
+            <ul>
+                {advantages.map((advantage, index) => (
+                    <li style={{listStyle: "none"}} key={index}>{advantage}</li>
+                ))}
+            </ul>
+        </main>
     );
 }
 
