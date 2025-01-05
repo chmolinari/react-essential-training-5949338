fd22c91a1824917015937338977f91b22ee8f95a
Christian Molinari <chmolinari@christianmolinari.com>
2025-01-05 20:32:59 +0100

    Variables
    Example on how to use variables in React components.
    
    Signed-off-by: Christian Molinari <chmolinari@christianmolinari.com>

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
 
+let tech = "React";
+let face = "😊";
+
 function App() {
-    return (
-        <h1>Hello React!</h1>
-    )
+    return <h1>Hello {tech.toUpperCase()}! {face}</h1>;
 }
 
 export default App
