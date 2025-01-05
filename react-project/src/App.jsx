079a859fe6da7025a423dd75e25bc22157c45034
Christian Molinari <chmolinari@christianmolinari.com>
2025-01-05 20:50:08 +0100

    Components
    Created my first component and my first JSX fragment inside an existing component.
    
    Signed-off-by: Christian Molinari <chmolinari@christianmolinari.com>

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -4,7 +4,22 @@ let tech = "React";
 let face = "😊";
 
 function App() {
-    return <h1>Hello {tech.toUpperCase()}! {face}</h1>;
+    return (
+        <>
+            <Header/>
+            <main>
+                <h2>First steps into React development</h2>
+            </main>
+        </>
+    );
+}
+
+function Header() {
+    return (
+        <header>
+            <h1>Hello {tech.toUpperCase()}!!! {face}</h1>
+        </header>
+    );
 }
 
 export default App
