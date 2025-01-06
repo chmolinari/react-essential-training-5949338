579eecf8448e12a4a630510bb1d8733b0015620f
Christian Molinari <chmolinari@christianmolinari.com>
2025-01-06 18:52:10 +0100

    Example of using a state object
    Implemented a way of managing the state of a component, using the useState hook.
    
    In this example, the state is applied to the component App, and passed to the component JobSearchStatus.
    
    Signed-off-by: Christian Molinari <chmolinari@christianmolinari.com>

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
 import logo from './images/logo_light.svg'
+import {useState} from "react";
 
 const tech = "React";
 const face = "😊";
 const author = "C. Molinari";
 const year = 2025;
+const activelySearchingState = "Actively searching";
+const notActivelySearchingState = "Not actively searching";
 
 const advantages = [
     {
@@ -22,10 +25,12 @@ const advantages = [
 ];
 
 function App() {
+    const [state, newState] = useState(notActivelySearchingState);
     return (
         <>
             <Header tech={tech} year="2025" author={author}/>
             <Main advantages={advantages}/>
+            <JobSearchStatus jobSearchState={state} newState={newState}/>
         </>
     );
 }
@@ -55,6 +60,27 @@ function Main({advantages}) {
 
 function Logo() {
     return <img width="170px" src={logo} alt="React logo"/>
+}
+
+function JobSearchStatus({jobSearchState, newState}) {
+    return (
+        <>
+            <h2>Job Search Status</h2>
+            <form>
+                <label form="jobSearchStatus">My current job search status is:</label>
+                <input type="text" name="jobSearchStatus" id="jobSearchStatus" value={jobSearchState} readOnly/>
+                <button type="button"
+                        onClick={() => newState(toggleJobSearchingState(jobSearchState))}>
+                    Change job search status
+                </button>
+            </form>
+        </>
+    )
+}
+
+function toggleJobSearchingState(currentState) {
+    if (currentState === notActivelySearchingState) return activelySearchingState;
+    return notActivelySearchingState;
 }
 
 export default App
